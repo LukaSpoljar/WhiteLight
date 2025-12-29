cb04451c8762dbf6dbb0332127f899b1100d0fc7
Ivan Luka Špoljar <lukispoljar@gmail.com>
2025-12-29 20:05:05 +0100

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App" onClick={() => {
+      if (!document.fullscreenElement) document.documentElement.requestFullscreen().catch((error: any) => console.error(`${error.message}`));
+      else document.exitFullscreen();
+    }}>
+    </div>
+  );
+}
+
+export default App;
