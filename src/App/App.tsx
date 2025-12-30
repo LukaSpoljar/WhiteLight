e77131460ac04ca2bcd762d5126cd153e705510a
Ivan Luka Špoljar <lukispoljar@gmail.com>
2025-12-30 11:39:01 +0100

    update

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,8 +3,12 @@ import './App.scss';
 function App() {
   return (
     <div className="App" onClick={() => {
-      if (!document.fullscreenElement) document.documentElement.requestFullscreen().catch((error: any) => console.error(`${error.message}`));
-      else document.exitFullscreen();
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch((error: any) => console.error(`${error.message}`))
+      }
+      else {
+        document.exitFullscreen()
+      };
     }}>
     </div>
   );
